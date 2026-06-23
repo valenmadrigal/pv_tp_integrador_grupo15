1cd6ba9d46aab5eb28ae02455d52ad6095957084
Lorena <lorenaaracelisuruguay@gmail.com>
2026-06-22 21:23:54 -0300

    conecta AdminProvider a la aplicacion

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,14 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import './index.css';
+import App from './App.jsx';
+import { AdminProvider } from './context/AdminContext';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <AdminProvider>
+      <App />
+    </AdminProvider>
   </StrictMode>,
-)
+);
