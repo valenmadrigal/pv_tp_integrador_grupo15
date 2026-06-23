f56415dda89dfd0600fe4e3b27ae463c922cc498
Lorena <lorenaaracelisuruguay@gmail.com>
2026-06-22 21:47:36 -0300

    Agrega persistencia de sesion con localStorage

--- src/context/AdminContext.jsx
+++ src/context/AdminContext.jsx
@@ -1,10 +1,21 @@
-import { createContext, useState } from "react";
+import { createContext, useState, useEffect } from "react";
 
 export const AdminContext = createContext();
 
 export const AdminProvider = ({ children }) => {
 
-  const [admin, setAdmin] = useState(null);
+  const [admin, setAdmin] = useState(() => {
+    const adminGuardado = localStorage.getItem("admin");
+    return adminGuardado ? JSON.parse(adminGuardado) : null;
+  });
+
+  useEffect(() => {
+    if (admin) {
+      localStorage.setItem("admin", JSON.stringify(admin));
+    } else {
+      localStorage.removeItem("admin");
+    }
+  }, [admin]);
 
   const login = (nombre, sector) => {
     setAdmin({
