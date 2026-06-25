e6e973ce78d11c94d51b7a44f6091bb8a1154f28
Lorena <lorenaaracelisuruguay@gmail.com>
2026-06-25 15:16:09 -0300

    se configura browserRouter para navegación

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,17 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
 import './index.css';
 import App from './App.jsx';
 import { AdminProvider } from './context/AdminContext';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <AdminProvider>
-      <App />
-    </AdminProvider>
+    <BrowserRouter>
+      <AdminProvider>
+        <App />
+      </AdminProvider>
+    </BrowserRouter>
   </StrictMode>,
 );
