3455a593ac961443199707731f8fc2ce9aaa4900
Lorena <lorenaaracelisuruguay@gmail.com>
2026-06-25 16:50:56 -0300

    se agrega la carpeta header y junto a ella se modifica app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,47 +1,97 @@
 import { Routes, Route, Navigate } from "react-router-dom";
 import { useContext } from "react";
+
 import { AdminContext } from "./context/AdminContext";
 
 import Login from "./views/Login";
 import ListaClientes from "./views/ListaClientes";
 import DetalleCliente from "./views/DetalleCliente";
 
+import Header from "./components/Header";
+
+
 function App() {
+
   const { admin } = useContext(AdminContext);
 
+
   return (
+
     <Routes>
 
+
+      
       <Route
         path="/login"
         element={
-          admin ? <Navigate to="/" /> : <Login />
+          admin 
+          ? <Navigate to="/" /> 
+          : <Login />
         }
       />
+
 
       <Route
         path="/"
         element={
-          admin ? <ListaClientes /> : <Navigate to="/login" />
+          admin 
+          ?
+          <>
+            <Header />
+            <ListaClientes />
+          </>
+          :
+          <Navigate to="/login" />
         }
       />
 
+
+      
       <Route
         path="/clientes"
         element={
-          admin ? <ListaClientes /> : <Navigate to="/login" />
+          admin
+          ?
+          <>
+            <Header />
+            <ListaClientes />
+          </>
+          :
+          <Navigate to="/login" />
         }
       />
 
+
+     
       <Route
         path="/clientes/:id"
         element={
-          admin ? <DetalleCliente /> : <Navigate to="/login" />
+          admin
+          ?
+          <>
+            <Header />
+            <DetalleCliente />
+          </>
+          :
+          <Navigate to="/login" />
         }
       />
 
+
+     
+      <Route
+        path="*"
+        element={
+          <Navigate to={admin ? "/" : "/login"} />
+        }
+      />
+
+
     </Routes>
+
   );
+
 }
+
 
 export default App;
---
+++ src/components/Header.jsx
@@ -0,0 +1,47 @@
+import { useContext } from "react";
+import { AdminContext } from "../context/AdminContext";
+
+function Header(){
+
+    const { admin, logout } = useContext(AdminContext);
+
+
+    return (
+        <header>
+
+            <h2>
+                Sistema Administrativo
+            </h2>
+
+
+            {
+                admin && (
+                    <div>
+
+                        <p>
+                            Usuario:
+                            {admin.nombre}
+                        </p>
+
+                        <p>
+                            Sector:
+                            {admin.sector}
+                        </p>
+
+
+                        <button onClick={logout}>
+                            Cerrar Sesión
+                        </button>
+
+
+                    </div>
+                )
+            }
+
+
+        </header>
+    )
+}
+
+
+export default Header;
