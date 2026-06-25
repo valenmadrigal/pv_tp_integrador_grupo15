3d996cde5be9de576c7297178c706f09715a0723
Lorena <lorenaaracelisuruguay@gmail.com>
2026-06-25 15:23:18 -0300

    configura rutas principales de la aplicación

---
+++ src/views/DetalleCliente.jsx
@@ -0,0 +1,9 @@
+function DetalleCliente() {
+  return (
+    <div>
+      <h1>Detalle del Cliente</h1>
+    </div>
+  );
+}
+
+export default DetalleCliente;
---
+++ src/views/ListaClientes.jsx
@@ -0,0 +1,9 @@
+function ListaClientes() {
+  return (
+    <div>
+      <h1>Lista de Clientes</h1>
+    </div>
+  );
+}
+
+export default ListaClientes;
