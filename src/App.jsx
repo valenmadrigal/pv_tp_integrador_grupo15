4211e3c34d5b3e7a4babbe38b8e04c9d3300bb4d
Lorena <lorenaaracelisuruguay@gmail.com>
2026-06-25 16:26:30 -0300

    Protege rutas mediante autenticación

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,45 @@
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, Navigate } from "react-router-dom";
+import { useContext } from "react";
+import { AdminContext } from "./context/AdminContext";
+
 import Login from "./views/Login";
 import ListaClientes from "./views/ListaClientes";
 import DetalleCliente from "./views/DetalleCliente";
 
 function App() {
+  const { admin } = useContext(AdminContext);
+
   return (
     <Routes>
-      <Route path="/login" element={<Login />} />
-      <Route path="/" element={<ListaClientes />} />
-      <Route path="/clientes" element={<ListaClientes />} />
-      <Route path="/clientes/:id" element={<DetalleCliente />} />
+
+      <Route
+        path="/login"
+        element={
+          admin ? <Navigate to="/" /> : <Login />
+        }
+      />
+
+      <Route
+        path="/"
+        element={
+          admin ? <ListaClientes /> : <Navigate to="/login" />
+        }
+      />
+
+      <Route
+        path="/clientes"
+        element={
+          admin ? <ListaClientes /> : <Navigate to="/login" />
+        }
+      />
+
+      <Route
+        path="/clientes/:id"
+        element={
+          admin ? <DetalleCliente /> : <Navigate to="/login" />
+        }
+      />
+
     </Routes>
   );
 }
