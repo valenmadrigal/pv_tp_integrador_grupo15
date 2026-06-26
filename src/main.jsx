63b85cbbf67161a29618ab9db3d32daacf46f7bd
valentina madrigal <valentinamdrgl@gmail.com>
2026-06-26 19:54:07 -0300

    cambio de nombre

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
-import './index.css';
+import './styles.css';
 import App from './App.jsx';
 import { AdminProvider } from './context/AdminContext';
 
